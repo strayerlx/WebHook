caed4a87d66f391fdff37fb9ef5ffe4c0cda6700
strayerlx <609903510@qq.com>
2023-11-24 23:34:29 +0800

    1

--- server01.sh
+++ server01.sh
@@ -7,9 +7,9 @@ git clean -f
 echo "拉取新代码"
 git pull origin master
 echo "开始构建"
-docker build -t server01 .
+docker build -t server01:1.0 .
 echo "停止旧容器并删除旧容器"
 docker stop server01-container
 docker rm server01-container
 echo "启动新容器"
-docker container run -p 3000:3000 --name server01-container -d server01
+docker container run -p 3000:3000 --name server01-container -d server01:1.0
