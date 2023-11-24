763bb01b614cdf3dc68ae006f306591f912caffe
strayerlx <609903510@qq.com>
2023-11-25 00:04:59 +0800

    1

--- webhook.js
+++ webhook.js
@@ -1,13 +1,14 @@
 let http = require('http')
 let crypto = require('crypto')
 const SECRET = "qwer1234@!~"
+let { spawn } = require('child_process')
 
 function sign(body) {
     return `sha1=` + crypto.createHmac('sha1', SECRET).update(body).digest('hex');
 }
 
 let server = http.createServer(function (req, res) {
-    console.log(req.method.req.url)
+    console.log(req.method, req.url)
     if (req.method == 'POST' && req.url == '/webhook') {
         let buffers = []
         req.on('data', function (buffer) {
@@ -15,14 +16,32 @@ let server = http.createServer(function (req, res) {
         })
         req.on('end', function (buffer) {
             let body = Buffer.concat(buffers)
-            let event = req.header['x-gitHub-event']
+            let event = req.headers['x-github-event']
             let signature = req.headers['x-hub-signature']
             if (signature !== sign(body)) {
                 return res.end("Not Allowed")
             }
+
+            res.setHeader('Content-Type', 'application/json')
+            res.end(JSON.stringify({ ok: true }))
+
+            if (event == 'push') {
+                let payload = JSON.parse(body)
+                let child = spawn('sh', [`./${payload.repository.name}.sh`])
+                let buffers = []
+                child.stdout.on('data', function (buffer) {
+                    buffers.push(buffer)
+                })
+
+                child.stdout.on('end', function (buffer) {
+                    let log = Buffer.concat(buffers);
+                    console.log(log)
+                })
+            }
         })
-        res.setHeader('Content-Type', 'application/json')
-        res.end(JSON.stringify({ ok: true }))
+
+
+
     } else {
         res.end('Not Found')
     }
